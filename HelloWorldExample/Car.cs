209a7c50a6adfaadbef0cefbb6c6d98e5af9d0ba
Bob Colts <rcolts34@gmail.com>
2025-01-14 20:01:06 -0500

    Updated file path

---
+++ HelloWorldExample/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldExample
+{
+    public class Car
+    {
+        public string make;
+        public string model;
+        public int year;
+        public string VIN;
+        public string exteriorColor;
+        public string interiorColor;
+
+        public Car(string make, string model, int year, string VIN, string exteriorColor, string interiorColor)
+        {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+            this.VIN = VIN;
+            this.exteriorColor = exteriorColor;
+            this.interiorColor = interiorColor;
+        }
+    }
+}
---
+++ HelloWorldExample/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorldExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // make a car
+            Car myBMW = new Car("BMW", "325xi", 2006, "WA345357", "Magnetic Grey", "Khaki");        // Constructor
+            // show info about the car on the label
+            label1.Text = myBMW.make.ToString();
+            List<Car> carList = new List<Car>();
+        }
+    }
+}
